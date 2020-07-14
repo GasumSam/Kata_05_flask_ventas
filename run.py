6f2a826dea2efdd9761911dc926907a830c41996
GasumSam <gasumsam@gmail.com>
2020-07-14 06:56:50 +0200

    json paises

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import csv
 
 app = Flask(__name__)
@@ -25,3 +25,23 @@ def index():
                 d[linea[0]] = {'ingresos': float(linea[11]), 'beneficios': float(linea[13])}
 
     return render_template('region.html', ventas=d)
+
+@app.route('/paises')
+def paises():
+    region_name = request.values['region']
+    
+    fVentas = open('./sales.csv', 'r')
+    csvreader = csv.reader(fVentas, delimiter= ',')
+    d = {}
+    for linea in csvreader:
+        if linea[0] == region_name:
+            if linea[1] in d:
+                d[linea[1]]['ingresos'] += float(linea[11])
+                d[linea[1]]['beneficios'] += float(linea[13])
+            else:
+                d[linea[1]] = {'ingresos': float(linea[11]), 'beneficios': float(linea[13])}
+
+    return d
+
+    #return #'Aquí irá un listado de países por región' + region_name
+

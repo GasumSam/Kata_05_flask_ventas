c7d2eeff9a80600d2aae9d2bb54ea9720954f2c6
GasumSam <gasumsam@gmail.com>
2020-07-16 21:14:06 +0200

    addproductos alta nueva formulario

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import csv, sqlite3
 
 app = Flask(__name__)
@@ -60,7 +60,14 @@ def addproduct():
     if request.method == 'GET':
         return render_template('newproduct.html') #me devuelves el newproduct.html vacío
     else:
-        return 'Debo grabar un registro con {}, {}, {}'.format(request.values['tipo_producto'], 
-                                                                request.values['precio_unitario'],
-                                                                request.values['coste_unitario'])
+        conn = sqlite3.connect(BASE_DATOS)
+        cur = conn.cursor()
+        query = "INSERT INTO productos (tipo_producto, precio_unitario, coste_unitario) values (?, ?, ?);"
+        datos = (request.values.get('tipo_producto'), request.values.get('precio_unitario'), request.values.get('coste_unitario'))
 
+        cur.execute(query, datos)
+
+        conn.commit()
+        conn.close()
+
+        return redirect(url_for("productos"))  #Ejecuta la función def productos()
